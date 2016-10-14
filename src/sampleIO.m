5b741de1ea22b2f5290f5665d1361d44d8ae8d8a
gonzalo <gibarsin@itba.edu.ar>
2016-10-14 16:15:48 -0300

    add read/write of wav files

---
+++ src/sampleIO.m
@@ -0,0 +1,21 @@
+function sample = readSample(sampleType, sampleNumber)
+	persistent sampleFolder = "./samples/";
+	persistent sampleName = ["sample" sampleType];
+	persistent sampleFormat = ".wav";
+	sampleNumber = num2str(sampleNumber);
+
+	samplePath = [sampleFolder sampleName sampleNumber sampleFormat];
+
+	sample = wavread(samplePath);
+end
+
+function sample = writeSample(sample, sampleType, sampleNumber)
+	persistent sampleFolder = "./samples/";
+	persistent sampleName = ["sample" sampleType];
+	persistent sampleFormat =  ".wav";
+	sampleNumber = num2str(sampleNumber);
+
+	samplePath = [sampleFolder sampleName sampleNumber sampleFormat];
+
+	wavwrite(sample, samplePath);
+end
